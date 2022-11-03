8ebf45ff5bc00e0b4a33efb5441b085109a4a1e4
arjun-myanger <admyanger@outlook.com>
2022-11-03 02:00:41 +0000

    prints key and value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    let arguments: Skip<Args> = std::env::args().skip(1);
+    let mut arguments = std::env::args().skip(1);
+    let key = arguments.next().unwrap();
+    let value = arguments.next().unwrap();
+    println!("The key is {} and the value is {}", key, value);
 }
