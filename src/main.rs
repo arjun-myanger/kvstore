4a113c503770ee44258c25c3b8b355063d113d51
arjun-myanger <admyanger@outlook.com>
2022-11-03 02:16:56 +0000

    More commits

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     let mut arguments = std::env::args().skip(1);
-    let key = arguments.next().unwrap();
+    let key = arguments.next().expect("Key not found");
     let value = arguments.next().unwrap();
     println!("The key is {} and the value is {}", key, value);
+    let contents = format!("{}\t{}\n", key, value);
+    std::fs::write("kv.db", contents).unwrap();
 }
